a30fd89bb6581617e201b9ed25725ef6fecf541d
shihabsaleem <shihabchikkushihab@gmail.com>
2025-07-13 15:42:09 +0400

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,32 +5,31 @@ const works = data.works;
 
 export default function Home() {
   return (
-    <div className="flex flex-col font-sans p-8 sm:p-10 gap-10">
+    <div className="flex flex-col font-sans p-4 sm:p-6 md:p-10 gap-10">
       {works.map((work) => (
         <div
           key={work.id}
-          className={`flex gap-4 max-w-screen w-full px-4 ${
-            work.id % 2 === 0 ? "flex-row-reverse" : ""
-          }`}
+          className={`flex flex-col-reverse lg:flex-row ${
+            work.id % 2 === 0 ? "lg:flex-row-reverse" : ""
+          } gap-6 max-w-screen w-full`}
         >
-          <div className="w-2/3 border-2 border-gray-900 p-6 rounded-3xl">
+          <div className="w-full lg:w-2/3 border-2 border-gray-900 p-4 rounded-3xl">
             <Image
               src={work.image}
               alt={work.name}
               className="w-full h-auto rounded-xl"
               width={600}
               height={400}
+              priority
             />
           </div>
 
-          <div className="w-1/3 border-2 border-gray-900 p-6 rounded-3xl space-y-2">
+          <div className="w-full lg:w-1/3 border-2 border-gray-900 p-6 rounded-3xl space-y-3">
             <div className="font-semibold text-lg text-white">{work.name}</div>
-            <div className="font-semibold text-sm text-neutral-400">
-              {work.shortdesc}
-            </div>
-            <div className="font-normal text-sm text-neutral-400 leading-relaxed">
+            <div className="font-semibold text-sm text-neutral-400">{work.shortdesc}</div>
+            <p className="font-normal text-sm text-neutral-400 leading-relaxed">
               {work.description}
-            </div>
+            </p>
           </div>
         </div>
       ))}
